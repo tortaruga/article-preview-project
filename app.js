5c19f1b9179a936eb4095cefa93bb66957fb84c3
tortaruga <134407215+tortaruga@users.noreply.github.com>
2024-06-14 20:01:03 +0200

    updated index.html, styles.css, app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const shareButton = document.getElementById('share-button');
+const tooltip = document.getElementById('tooltip');
+const shareIcons = document.getElementById('share-icons');
+const authorInfo = document.querySelector('.author-info');
+const articleFooter = document.getElementById('article-footer');
+
+shareButton.addEventListener('click', () => {
+    if (window.matchMedia('(min-width: 728px)').matches) {
+       tooltip.classList.toggle('hidden');
+       shareButton.classList.toggle('change-background');
+       if (shareButton.src.includes('icon-share-white.svg')) {
+           shareButton.src = './images/icon-share.svg';
+       } else {
+        shareButton.src = './images/icon-share-white.svg';
+       }       
+    } else {
+      shareIcons.classList.toggle('hidden');
+      authorInfo.classList.toggle('hidden');
+      articleFooter.classList.toggle('change-background');
+    }
+}
+)
