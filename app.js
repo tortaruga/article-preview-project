7cc65011a72ee3c7e1a192e849a2552f4d42db18
tortaruga <134407215+tortaruga@users.noreply.github.com>
2024-06-16 15:49:59 +0200

    updated app.js

--- app.js
+++ app.js
@@ -4,19 +4,35 @@ const shareIcons = document.getElementById('share-icons');
 const authorInfo = document.querySelector('.author-info');
 const articleFooter = document.getElementById('article-footer');
 
+// shareButton.addEventListener('click', () => {
+//     if (window.matchMedia('(min-width: 728px)').matches) {
+//        tooltip.classList.toggle('hidden');
+//        shareButton.classList.toggle('change-background');
+//        if (shareButton.src.includes('icon-share-white.svg')) {
+//            shareButton.src = './images/icon-share.svg';
+//        } else {
+//         shareButton.src = './images/icon-share-white.svg';
+//        }       
+//     } else {
+//       shareIcons.classList.toggle('hidden');
+//       authorInfo.classList.toggle('hidden');
+//       articleFooter.classList.toggle('change-background');
+//     }
+// }
+// )
+
 shareButton.addEventListener('click', () => {
-    if (window.matchMedia('(min-width: 728px)').matches) {
-       tooltip.classList.toggle('hidden');
+  if (window.innerWidth >= 728) {
+    tooltip.classList.toggle('hidden');
        shareButton.classList.toggle('change-background');
        if (shareButton.src.includes('icon-share-white.svg')) {
            shareButton.src = './images/icon-share.svg';
        } else {
         shareButton.src = './images/icon-share-white.svg';
-       }       
-    } else {
+       }
+  } else {
       shareIcons.classList.toggle('hidden');
       authorInfo.classList.toggle('hidden');
       articleFooter.classList.toggle('change-background');
     }
-}
-)
+})
